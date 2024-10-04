edfe124e1f48038671b5ca451ef421b74f459114
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-04 19:46:14 +0200

    add findByTitleContains method

--- src/main/java/com/ticket/java/repository/TicketRepository.java
+++ src/main/java/com/ticket/java/repository/TicketRepository.java
@@ -1,9 +1,13 @@
 package com.ticket.java.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ticket.java.model.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+	public List<Ticket> findByTitleContains(String title);
 
 }
