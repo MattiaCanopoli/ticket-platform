9abf6677f67202cd28095c1f175fc73568ba77cb
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-05 01:35:04 +0200

    Create UserService.java

---
+++ src/main/java/com/ticket/java/service/UserService.java
@@ -0,0 +1,22 @@
+package com.ticket.java.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ticket.java.model.User;
+import com.ticket.java.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository uRepo;
+
+	public List<User> findAll() {
+		List<User> users = uRepo.findAll();
+		return users;
+	}
+
+}
