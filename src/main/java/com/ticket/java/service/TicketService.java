3a7e133db23ab1f6bd2e744841e240cf77c5f493
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-04 19:46:32 +0200

    add: findAll and findByTitle methods

--- src/main/java/com/ticket/java/service/TicketService.java
+++ src/main/java/com/ticket/java/service/TicketService.java
@@ -1,8 +1,11 @@
 package com.ticket.java.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ticket.java.model.Ticket;
 import com.ticket.java.repository.TicketRepository;
 
 @Service
@@ -10,5 +13,15 @@ public class TicketService {
 
 	@Autowired
 	TicketRepository tRepo;
+
+	public List<Ticket> findAll() {
+		List<Ticket> tickets = tRepo.findAll();
+		return tickets;
+	}
+
+	public List<Ticket> findByTitle(String title) {
+		List<Ticket> tickets = tRepo.findByTitleContains(title);
+		return tickets;
+	}
 
 }
