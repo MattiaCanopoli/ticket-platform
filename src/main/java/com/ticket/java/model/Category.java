cf253a675c9835ed7e35d299b678d7915aab9649
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-04 19:45:07 +0200

    add: getters and setters; ticket relationship

--- src/main/java/com/ticket/java/model/Category.java
+++ src/main/java/com/ticket/java/model/Category.java
@@ -1,9 +1,12 @@
 package com.ticket.java.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
@@ -18,4 +21,25 @@ public class Category {
 	@NotNull
 	@NotEmpty
 	private String name;
+
+	@OneToMany(mappedBy = "category")
+	private List<Ticket> ticket;
+
+	// getter & setter
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
