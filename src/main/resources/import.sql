f5faa5188c69c58496bb432430bd0f6e73321b2a
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-05 18:52:53 +0200

    removed category import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,3 @@
 INSERT INTO user (email, name, password) VALUES ('mattia.canopoli@ticket.com','mattia.canopoli','{noop}password'),('mario.rossi@ticket.com', 'mario.rossi','{noop}password'), ('generic.user@ticket.com','generic.user','{noop}password'),('generic.admin@ticket.com','generic.admin','{noop}password');
 INSERT INTO role (role_name) VALUES ('ADMIN'),('USER');
 INSERT INTO user_roles (user_id, roles_id) VALUES (1,1),(2,2),(3,2),(4,1);
-INSERT INTO category (name) VALUES ('not urgent'),('urgent'),('blocking')
