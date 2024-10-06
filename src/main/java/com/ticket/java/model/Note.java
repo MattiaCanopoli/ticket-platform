85d9ec02f881a1594fcf21362917c246f1ffdacc
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-06 21:39:55 +0200

    removed notes/user relation for testing

--- src/main/java/com/ticket/java/model/Note.java
+++ src/main/java/com/ticket/java/model/Note.java
@@ -1,5 +1,10 @@
 package com.ticket.java.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
@@ -22,13 +28,20 @@ public class Note {
 	@Column(columnDefinition = "TEXT")
 	private String content;
 
-	@ManyToOne
-	@JoinColumn(name = "author", nullable = false)
-	private User author;
+//	@ManyToOne
+//	@JoinColumn(name = "author", nullable = false)
+//	private User author;
 
 	@ManyToOne
 	@JoinColumn(name = "ticket_id", nullable = false)
 	private Ticket ticket;
+
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Transient
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:MM");
 
 	// getter & setter
 
@@ -48,13 +61,13 @@ public class Note {
 		this.content = content;
 	}
 
-	public User getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(User author) {
-		this.author = author;
-	}
+//	public User getAuthor() {
+//		return author;
+//	}
+//
+//	public void setAuthor(User author) {
+//		this.author = author;
+//	}
 
 	public Ticket getTicket() {
 		return ticket;
@@ -62,6 +75,22 @@ public class Note {
 
 	public void setTicket(Ticket ticket) {
 		this.ticket = ticket;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getFormattedDate() {
+		if (this.createdAt != null) {
+			return this.createdAt.format(formatter);
+		}
+
+		return null;
 	}
 
 }
