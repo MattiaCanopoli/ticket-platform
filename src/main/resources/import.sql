4cb2e86390230dbbf975a9daf8201a838bdffeca
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-09 00:19:47 +0200

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO user (email, name,lastname, username, password, available) VALUES ('mattia.canopoli@ticket.com','mattia','canopoli','mattia.canopoli','{noop}password',true),('mario.rossi@ticket.com','mario','rossi','mario.rossi','{noop}password',true),('generic.user@ticket.com','generic','user','generic.user','{noop}password',false),('generic.admin@ticket.com','generic','admin','generic.admin','{noop}password',true);
+INSERT INTO user (email, name,lastname, username, password, available) VALUES ('mattia.canopoli@ticket.com','mattia','canopoli','mattia.canopoli','{noop}password',true),('mario.rossi@ticket.com','mario','rossi','mario.rossi','{noop}password',true),('generic.user@ticket.com','generic','user','generic.user','{noop}password',true),('generic.admin@ticket.com','generic','admin','generic.admin','{noop}password',true);
 INSERT INTO role (role_name) VALUES ('ADMIN'),('USER');
 INSERT INTO user_roles (user_id, roles_id) VALUES (1,1),(2,2),(3,2),(4,1);
 INSERT INTO category (id,name) VALUES (1,'bug fix'),(2,'support'),(3,'new implementation'),(4,'meeting request');
