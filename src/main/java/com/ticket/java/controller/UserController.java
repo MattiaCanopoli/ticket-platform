eceed0e035b2f3ff7de01ae576d61ed1cd7a2cac
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-08 13:21:15 +0200

    add: tickets to model

--- src/main/java/com/ticket/java/controller/UserController.java
+++ src/main/java/com/ticket/java/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.ticket.java.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ticket.java.service.TicketService;
 import com.ticket.java.service.UserService;
 
 @Controller
@@ -16,9 +18,13 @@ public class UserController {
 	@Autowired
 	UserService uService;
 
+	@Autowired
+	TicketService tService;
+
 	@GetMapping("/{id}")
-	public String userPage(Model model, @PathVariable("id") Integer id) {
-		model.addAttribute("user", uService.getById(id));
+	public String userPage(Model model, @PathVariable("id") Integer id, Authentication auth) {
+		model.addAttribute("user", uService.getByUsername(auth.getName()));
+		model.addAttribute("tickets", tService.findUserTickets(id));
 		return "/users/user";
 	}
 
