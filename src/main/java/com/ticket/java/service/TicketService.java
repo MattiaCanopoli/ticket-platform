89fc12ebd355ff6932dcb4b455f4503d364043f3
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-10 18:09:33 +0200

    disabled user change available on ticket create

--- src/main/java/com/ticket/java/service/TicketService.java
+++ src/main/java/com/ticket/java/service/TicketService.java
@@ -43,7 +43,7 @@ public class TicketService {
 	}
 
 	public Ticket save(Ticket ticket) {
-		ticket.getUser().setAvailable(false);
+		// ticket.getUser().setAvailable(false);
 		return tRepo.save(ticket);
 	}
 
