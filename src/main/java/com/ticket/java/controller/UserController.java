24dafa6af455a0963dbf1031f758f1e02082261e
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-12 12:21:16 +0200

    add: exceptions

--- src/main/java/com/ticket/java/controller/UserController.java
+++ src/main/java/com/ticket/java/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ticket.java.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ticket.java.model.User;
@@ -26,19 +28,33 @@ public class UserController {
 
 	@GetMapping("/{id}")
 	public String userPage(Model model, @PathVariable("id") Integer id, Authentication auth) {
+
+		User currentUser = uService.getByUsername(auth.getName());
+
+		if (id != currentUser.getId()) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You are not allowed here");
+		}
+
 		model.addAttribute("currentUser", uService.getByUsername(auth.getName()));
 		// model.addAttribute("tickets", tService.findUserTickets(id));
 		return "/users/user";
 	}
 
 	@PostMapping("/changestatus/{id}")
-	public String changeUserStatus(@PathVariable("id") Integer id, RedirectAttributes feedback) {
+	public String changeUserStatus(@PathVariable("id") Integer id, RedirectAttributes feedback, Authentication auth) {
+
+		Integer currentUserId = uService.getByUsername(auth.getName()).getId();
+
+		if (id != currentUserId) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You are not allowed here");
+		}
 
 		User user = uService.getById(id);
 		Boolean userStatus = user.isActive();
 		Integer openTickets = user.getOpenTickets();
 
-		// user is active (true) and has no open tickets -> status change to
+		// user is active (true) and has no open tickets OR user is non active (false)
+		// and has open tickets -> status change to
 		// non-active(false)
 		if ((userStatus && openTickets == 0) || (!userStatus && openTickets > 0)) {
 			user.setActive(!userStatus);
@@ -47,41 +63,16 @@ public class UserController {
 		}
 
 		// user is active (true) and has open tickets -> cannot change status
-		if (userStatus && openTickets > 0) {
+		else if (userStatus && openTickets > 0) {
 			feedback.addFlashAttribute("dangerMessage", "Cannot change status now, you still have open tickets");
 		}
 
 		// user is non active (false) and doesn't have open tickets -> cannot change
 		// status
-		if (!userStatus && openTickets == 0) {
+		else if (!userStatus && openTickets == 0) {
 			feedback.addFlashAttribute("dangerMessage", "Cannot change status now, you dont have any open ticket");
 		}
-		// user is non active (false) and doesn't have open tickets -> cannot change
-//		if (!userStatus && openTickets > 0) {
-//			user.setActive(true);
-//			uService.save(user);
-//			feedback.addFlashAttribute("successMessage", "Status successfully updated");
-//		}
 
-//
-//		if (user.isActive()) {
-//			user.setActive(false);
-//			feedback.addFlashAttribute("successMessage", "Status successfully updated");
-//		}
-//
-//		if (user.getOpenTickets() == 0 && !user.isActive()) {
-//			user.setActive(true);
-//			feedback.addFlashAttribute("successMessage", "Status successfully updated");
-//		}
-//
-//		if (user.getOpenTickets() > 0 && !user.isActive()) {
-//			feedback.addFlashAttribute("dangerMessage", "Cannot chance status now, you still have open tickets");
-//			return "redirect:/users/{id}";
-//		}
-//
-//		feedback.addFlashAttribute("successMessage", "Status successfully updated");
-//		user.setActive(true);
-//		uService.save(user);
 		return "redirect:/users/{id}";
 
 	}
