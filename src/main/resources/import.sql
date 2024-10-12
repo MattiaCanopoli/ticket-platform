02cd75aff19ff46242d5b3c52109a284a472fc1b
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-10-12 20:01:25 +0200

    add: new user gianni verdi

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO user (email, name,lastname, username, password, active) VALUES ('mattia.canopoli@ticket.com','mattia','canopoli','mattia.canopoli','{noop}password',false),('mario.rossi@ticket.com','mario','rossi','mario.rossi','{noop}password',false),('generic.user@ticket.com','generic','user','generic.user','{noop}password',false),('generic.admin@ticket.com','generic','admin_user','generic.admin_user','{noop}password',false);
+INSERT INTO user (email, name,lastname, username, password, is_active) VALUES ('mattia.canopoli@ticket.com','mattia','canopoli','mattia.canopoli','{noop}password',false),('mario.rossi@ticket.com','mario','rossi','mario.rossi','{noop}password',false),('generic.user_admin@ticket.com','generic','user_admin','generic.user_admin','{noop}password',false),('generic.admin@ticket.com','generic','admin_user','generic.admin_user','{noop}password',false),('gianni.verdi@ticket.com','gianni','verdi','gianni.verdi','{noop}password',false);
 INSERT INTO role (role_name) VALUES ('ADMIN'),('USER');
-INSERT INTO user_roles (user_id, roles_id) VALUES (1,1),(2,2),(3,2),(4,1),(4,2);
 INSERT INTO category (id,name) VALUES (1,'bug fix'),(2,'support'),(3,'new implementation'),(4,'meeting request');
-INSERT INTO ticket_status (id,name) VALUES (1,'OPEN'),(2,'IN PROGRESS'),(3,'COMPLETED');
+INSERT INTO status (id,name) VALUES (1,'OPEN'),(2,'IN PROGRESS'),(3,'COMPLETED');
+INSERT INTO user_roles (user_id, roles_id) VALUES (1,1),(2,2),(3,2),(3,1),(4,1),(4,2),(5,2);
